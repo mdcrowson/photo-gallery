d1c599fe67245db8a1528d1522dbeab3018d86fb
mdcrowson <marissa.crowson23@gmail.com>
2022-03-25 14:39:31 -0500

    Update search filter
    Simplified by adding a for loop.

--- js/searchFilterMDC.js
+++ js/searchFilterMDC.js
@@ -40,18 +40,75 @@
 // let caption1 = document.getElementById('photo1');   
 // console.log(caption1);
 
+// function searchFilter() {
+//     let userInput = document.getElementById('search').value.toUpperCase();
+//     console.log(userInput);
+//     const regex1 = new RegExp ('\\b' + userInput, 'i');
+    
+//     if (regex1.test("I love hay bales Took this snap on a drive through the countryside past some straw fields ")) {
+//         document.getElementById('photo1').style.display = "initial";
+//     } else document.getElementById('photo1').style.display = "none";
+    
+//     if (regex1.test("The lake was so calm today We had a great view of the snow on the mountains from here ")) {
+//         document.getElementById('photo2').style.display = "initial";
+//     } else document.getElementById('photo2').style.display = "none";
+// }
+
+// function searchFilter() {
+//     let userInput = document.getElementById('search').value.toUpperCase();
+//     console.log(userInput);
+//     let userSubstrings = userInput.split(' ');
+//     // const regex1 = new RegExp ('\\b' + userSubstrings, 'i');
+//     const regex1 = new RegExp ('\\b' + userSubstrings, 'i');
+    
+//     if (regex1.test("I love hay bales. Took this snap on a drive through the countryside past some straw fields.")) {
+//         document.getElementById('photo1').style.display = "initial";
+//     } else document.getElementById('photo1').style.display = "none";
+
+// }
+
+// function searchFilter() {
+//     let userInput = document.getElementById('search').value;
+//     // console.log(userInput);
+//     let splitInput = userInput.split(' ');
+//     // console.log(userSubstrings);
+    
+//     // userSubstrings.forEach(element => {
+//     //     let userRegex = userSubstrings.map(element => '\\b' + element);
+//     //     console.log(userRegex);
+//     // }
+//     // );
+   
+
+//     // let userRegex = userSubstrings.map(element => '\\b' + element);
+//     // console.log(userRegex);
+    
+//     let comboRegex = splitInput.join('|');
+//     console.log(comboRegex);
+
+// }
+
 function searchFilter() {
-    let userInput = document.getElementById('search').value.toUpperCase();
-    console.log(userInput);
-    const regex1 = new RegExp ('\\b' + userInput, 'i');
+    let userInput = document.getElementById('search').value;
+
+    // dynamic regex idea from Tomas Langkaas at https://stackoverflow.com/a/41869757
+    let regex = new RegExp("(?=.*?\\b" + 
+        userInput
+            .split(" ")
+            .join(")(?=.*?\\b") +                     
+        ").*", 
+        "i"
+        );
     
-    if (regex1.test("I love hay bales Took this snap on a drive through the countryside past some straw fields ")) {
-        document.getElementById('photo1').style.display = "initial";
-    } else document.getElementById('photo1').style.display = "none";
+    for (let i=1; i<=12; i++) {
+        if (regex.test(document.getElementById(`photo${i}`).getAttribute('data-caption'))) {
+                document.getElementById(`photo${i}`).style.display = "initial";
+            } else document.getElementById(`photo${i}`).style.display = "none";
+    }      
     
-    if (regex1.test("The lake was so calm today We had a great view of the snow on the mountains from here ")) {
-        document.getElementById('photo2').style.display = "initial";
-    } else document.getElementById('photo2').style.display = "none";
 }
 
 
+// if (regex.test(document.getElementById('photo1').getAttribute('data-caption'))) {
+//     document.getElementById('photo1').style.display = "initial";
+// } else document.getElementById('photo1').style.display = "none";
