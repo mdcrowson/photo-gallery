94187ecfb82f15315e871d987dc2d18fde030828
mdcrowson <marissa.crowson23@gmail.com>
2022-03-25 09:51:16 -0500

    Updates regex1 in searchFilterMDC.js
    Search filter works for first two photos, but only if user input matches caption words in order.

--- js/searchFilterMDC.js
+++ js/searchFilterMDC.js
@@ -19,18 +19,39 @@
 //     }
 //   });
 
-function searchFilter() {
-    let userInput = document.getElementById('search').value.toUpperCase();
-    console.log(userInput);
-    const regex1 = new RegExp (('HAY'));
-    if (regex1.test(userInput)) {
-        return document.getElementById('photo1').style.display = "initial";
-    } else return document.getElementById('photo1').style.display = "none";
-}
+// function searchFilter() {
+//     let userInput = document.getElementById('search').value;
+//     console.log(userInput);
+//     const regex1 = new RegExp (/(?:^|\s)hay/i|(?:^|\s)bales/i);
+//     if (regex1.test(userInput)) {
+//         return document.getElementById('photo1').style.display = "initial";
+//     } else return document.getElementById('photo1').style.display = "none";
+// }
+
+// function searchFilter() {
+//     let userInput = document.getElementById('search').value.toUpperCase();
+//     console.log(userInput);
+//     const regex1 = new RegExp ('\\b' + userInput, 'i');
+//     if (regex1.test("I love hay bales Took this snap on a drive through the countryside past some straw fields ")) {
+//         return document.getElementById('photo1').style.display = "initial";
+//     } else return document.getElementById('photo1').style.display = "none";
+// }
 
 // let caption1 = document.getElementById('photo1');   
 // console.log(caption1);
 
-
+function searchFilter() {
+    let userInput = document.getElementById('search').value.toUpperCase();
+    console.log(userInput);
+    const regex1 = new RegExp ('\\b' + userInput, 'i');
+    
+    if (regex1.test("I love hay bales Took this snap on a drive through the countryside past some straw fields ")) {
+        document.getElementById('photo1').style.display = "initial";
+    } else document.getElementById('photo1').style.display = "none";
+    
+    if (regex1.test("The lake was so calm today We had a great view of the snow on the mountains from here ")) {
+        document.getElementById('photo2').style.display = "initial";
+    } else document.getElementById('photo2').style.display = "none";
+}
 
 
